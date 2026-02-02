6dd7c61d1422b102f291ccfe172ff65e713c4622
Milan <76396495+themilan1337@users.noreply.github.com>
2026-02-02 20:42:52 +0500

    321

--- landing/app/app.vue
+++ landing/app/app.vue
@@ -1,5 +1,10 @@
 <template>
   <NuxtLayout>
+    <Analytics/>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
