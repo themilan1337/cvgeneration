b07130308b5bfd3f9a43271c27ba744177cd11c7
Milan <76396495+themilan1337@users.noreply.github.com>
2026-02-02 20:37:01 +0500

    321

--- landing/nuxt.config.ts
+++ landing/nuxt.config.ts
@@ -1,12 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  ssr: true,
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: [
     '@nuxt/a11y',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/image'
   ]
-})
+});
